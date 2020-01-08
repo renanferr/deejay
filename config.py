aa313e974ee6c3d0662eb0377e827ca3aaedd92f
Renan Ferreira <renanferreira.js@gmail.com>
2020-01-08 10:10:54 -0300

    Adds config by environment variables

--- config.py
+++ config.py
@@ -8,7 +8,11 @@ defaults = {
 class Config():
     def __init__(self):
         for k in defaults:
-            self.__setattr__(k, defaults.get(k))
+            if os.getenv(k) is not None:
+                self.__setattr__(k, os.getenv(k))
+            else:
+                self.__setattr__(k, defaults.get(k))
+        
         self.OUT_TMP = os.path.join(self.TMP_DIR, '%(id)s.%(ext)s')
 
 config = Config()
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from playback import Track
 from ytdl import download_track
 from config import config
 from time import time
-from utils import get_file_path, clear_dir
+from utils import clear_dir
 import os
 
 url = 'https://www.youtube.com/watch?v=BME88lS6aVY'
@@ -11,7 +11,6 @@ url = 'https://www.youtube.com/watch?v=BME88lS6aVY'
 if __name__ == '__main__':
     clear_dir(dir_path=config.TMP_DIR)
     try:
-        # tmp_file =  #'{}{}.%(ext)s'.format(config.TMP_FILE, time())
         out_file = config.OUT_TMP
         track_info = download_track(url, out_file=out_file, codec=config.CODEC)
         file_path = config.OUT_TMP % dict(id=track_info.get('id'), ext=config.CODEC)
