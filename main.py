d3532a461eafe9a54e6d6eb6e5cd8ef8d35ca046
Renan Ferreira <renanferreira.js@gmail.com>
2020-01-10 09:39:16 -0300

    Starts socket.io server implementation

--- main.py
+++ main.py
@@ -7,17 +7,9 @@ from server import app
 import os
 import eventlet
 
-url = 'https://www.youtube.com/watch?v=BME88lS6aVY'
+url = 'https://www.youtube.com/watch?v=POH14-HMGFc'
 
 
 if __name__ == '__main__':
-    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
     clear_dir(dir_path=config.TMP_DIR)
-    try:
-        out_file = config.OUT_TMP
-        track_info = download_track(url, out_file=out_file, codec=config.CODEC)
-        file_path = config.OUT_TMP % dict(id=track_info.get('id'), ext=config.CODEC)
-        t = Track(file_path=file_path, codec=config.CODEC, info=track_info)
-        t.play()
-    except Exception as e:
-        print(e)
+    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
